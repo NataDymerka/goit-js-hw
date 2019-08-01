583c9c6c80738b25e72fd05bf869bf1eb83e8402
NataDymerka <@N_A_D@ukr.net>
2019-08-01 14:31:17 +0300

    task-5: unnecessary variables removing

--- HW-01/task-5.js
+++ HW-01/task-5.js
@@ -1,5 +1,4 @@
 let country = prompt('Введите страну доставки');
-let deliveryPrice;
 let chinaDeliveryPrice = 100;
 let chileDeliveryPrice = 250;
 let australiaDeliveryPrice = 170;
@@ -13,24 +12,19 @@ const jamaica = 'Ямайка';
 if (country !== null) {
     switch (country.toLowerCase()) {
         case china.toLowerCase():
-            deliveryPrice = chinaDeliveryPrice;
-            alert(`Доставка в ${china} будет стоить ${deliveryPrice} кредитов`);
+            alert(`Доставка в ${china} будет стоить ${chinaDeliveryPrice} кредитов`);
             break;
         case chile.toLowerCase():
-            deliveryPrice = chileDeliveryPrice;
-            alert(`Доставка в ${chile} будет стоить ${deliveryPrice} кредитов`)
+            alert(`Доставка в ${chile} будет стоить ${chileDeliveryPrice} кредитов`);
             break;
         case australia.toLowerCase():
-            deliveryPrice = australiaDeliveryPrice;
-            alert(`Доставка в ${australia} будет стоить ${deliveryPrice} кредитов`)
+            alert(`Доставка в ${australia} будет стоить ${australiaDeliveryPrice} кредитов`);
             break;
         case india.toLowerCase():
-            deliveryPrice = indiaDeliveryPrice;
-            alert(`Доставка в ${india} будет стоить ${deliveryPrice} кредитов`)
+            alert(`Доставка в ${india} будет стоить ${indiaDeliveryPrice} кредитов`);
             break;
         case jamaica.toLowerCase():
-            deliveryPrice = jamaicaDeliveryPrice;
-            alert(`Доставка в ${jamaica} будет стоить ${deliveryPrice} кредитов`)
+            alert(`Доставка в ${jamaica} будет стоить ${jamaicaDeliveryPrice} кредитов`);
             break;
         default:
             alert('В вашей стране доставка не доступна');
