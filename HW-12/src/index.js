a6a4d059337f04ba184be2de2b1faa4995bac9df
NataDymerka <nata.dymerka@gmail.com>
2019-11-06 21:30:52 +0200

    HW-12 files add

---
+++ HW-12/src/index.js
@@ -0,0 +1,55 @@
+import './styles.css';
+import countryTemplate from './templates/countryDescription.hbs';
+import PNotify from '../node_modules/pnotify/dist/es/PNotify.js';
+
+const debounce = require('lodash.debounce');
+
+const countryInput = document.querySelector('#country-input');
+const countryList = document.querySelector('#countryList');
+const countryDescr = document.querySelector('#country-description');
+
+countryInput.addEventListener('input', debounce(inputHandler, 500));
+
+function inputHandler(e) {
+    let input = e.target.value;
+    fetchCounties(input);
+}
+
+function fetchCounties(input) {
+    const baseUrl = 'https://restcountries.eu/rest/v2/name/';
+
+    fetch(baseUrl + input).then(response => {
+        return response.json()
+
+    }).then(data => {
+        clear();
+        if (data.length > 10) {
+            message();
+        } else if (data.length == 1) {
+            console.log(data);
+            renderCountryDescr(data[0]);
+
+        } else if (data.length > 1 && data.length <= 10) {
+            console.log(data);
+            renderCountryList(data);
+        }
+    }).catch(error => console.log(error));
+}
+
+function clear() {
+    countryList.innerHTML = '';
+    countryDescr.innerHTML = '';
+}
+
+function message() {
+    PNotify.alert('There are too many variants! Please, specify your search!');
+}
+
+function renderCountryList(countries) {
+    const markup = countries.map(country => `<li class="country-name_item">${country.name}</li>`).join('');
+    countryList.insertAdjacentHTML('beforeend', markup);
+}
+
+function renderCountryDescr(obj) {
+    countryDescr.insertAdjacentHTML('beforeend', countryTemplate(obj));
+}
